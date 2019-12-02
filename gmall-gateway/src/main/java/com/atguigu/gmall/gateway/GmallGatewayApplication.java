24582f8e857766321fc85b3b5fdbe7f7ab169014
Chauuuuu <188376728@qq.com>
2019-12-02 18:59:05 +0800

    项目基本环境搭建

--- gmall-gateway/src/main/java/com/atguigu/gmall/gateway/GmallGatewayApplication.java
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/GmallGatewayApplication.java
@@ -1,21 +1,17 @@
-package com.atguigu.gmall.pms;
+package com.atguigu.gmall.gateway;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @RefreshScope
-@MapperScan(basePackages = "com.atguigu.gmall.pms.dao")
-@EnableSwagger2
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GmallPmsApplication {
+public class GmallGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GmallPmsApplication.class, args);
+		SpringApplication.run(GmallGatewayApplication.class, args);
 	}
 
 }
